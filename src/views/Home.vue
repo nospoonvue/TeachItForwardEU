b08ca12e94a56daafd7baa4cd97a0a14cc0c3af0
nospoonvue <nospoonvue@nospoon.nl>
2022-03-13 00:31:15 +0000

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,56 @@
+<template>
+    <div id="blog">    
+        <GenericPage class="" :page="page" :paging="paging" :pageSize="pageSize" :translateTitle="translateTitle"/>
+  
+    </div>
+</template>
+
+
+<style >
+
+</style>
+
+<script>
+
+import GenericPage from '@/components/GenericPage.vue'
+
+export default 
+{
+    name: "genericPage",
+    components: {GenericPage},
+
+    data() 
+    {
+        return {
+        page: "Home",
+        pageSize: 1,
+        paging: false,
+        translateTitle: false
+        }
+    },
+    metaInfo() {
+        return {
+            title: 'Starbase18 - ' + this.page,
+            meta:[
+                {
+                    name: "Empowering students to thrive in a circular economy",
+                    content: "Building a circular mini vilage in central Portugal"
+                }
+            ]
+      }
+    },
+    methods:{
+
+    },
+    computed: {
+
+    },
+    created(){
+
+    },
+}
+
+
+</script>
+
+
